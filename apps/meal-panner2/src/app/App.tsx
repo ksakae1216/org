b1f7e128e73b98a34810998675aee36b425a4bf4
kotaro_sakae <ksakae1216@gmail.com>
2025-02-18 18:53:24 +0900

    作る人がログインするときにgroupを作成

--- apps/meal-panner2/src/app/App.tsx
+++ apps/meal-panner2/src/app/App.tsx
@@ -1,4 +1,5 @@
-import auth from '@react-native-firebase/auth';
+import { FirebaseAuthTypes, getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
+import { enableNetwork, getFirestore } from '@react-native-firebase/firestore';
 import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -8,7 +9,21 @@ import { AppNavigator } from '../navigation/AppNavigator';
 
 export function App() {
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged((user) => {
+    const initializeFirebase = async () => {
+      try {
+        const auth = getAuth();
+        const db = getFirestore(auth.app, 'meal-planner-db');
+
+        await enableNetwork(db);
+      } catch (error) {
+        console.error('Failed to initialize Firestore:', error);
+      }
+    };
+
+    initializeFirebase();
+
+    const auth = getAuth();
+    const subscriber = onAuthStateChanged(auth, (user: FirebaseAuthTypes.User | null) => {
       console.log('Auth state changed:', user);
     });
 
